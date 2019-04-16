1b485838fba785535763eac41ec6857d14f6efa2
amantix <anton.a.marchenko@gmail.com>
2019-04-16 23:07:57 +0300

    Добавили класс пользователя, настроили контекст, выводим имена всех пользователей из банки

---
+++ TechShowdown/Migrations/20190416184934_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TechShowdown.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("b32b790d-f9cb-49eb-9e7b-4e6132592084"), "John Snow" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("62ad51cc-7475-48bb-b7a8-afe5631cd26f"), "Sarah Connor" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- TechShowdown/Program.cs
+++ TechShowdown/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace TechShowdown
 {
+    /*
+     * Добавим класс для хранения данных о пользователе
+     * Пусть для начала он будет содержать идентификатор и имя
+     */
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    /*
+     * Добавим также контекст базы данных, содержащей таблицу с нашими пользователями
+     * Класс наследуется от DbContext
+     */
+    public sealed class AppDb : DbContext
+    {
+        /*
+         * Пример строки подключения к локальной базе mssqllocaldb
+         * Server=(localdb)\mssqllocaldb;Database=AppDb;AttachDbFilename=E:\AppDb.mdf;Trusted_Connection=True;ConnectRetryCount=0
+         */
+        private readonly string connectionString;
+
+        public AppDb(string connectionString)
+        {
+            this.connectionString = connectionString;
+            Database.EnsureCreated(); //.Migrate();
+        }
+
+        //Коллекция DbSet<User> будет использоваться для работы с пользователями
+        public DbSet<User> Users { get; set; }
+
+        /*
+         * В перегруженном методе OnConfiguring настраиваем подключение к базе данных
+         * Пример строки подключения:
+         * Server=(localdb)\mssqllocaldb;Database=AppDb;AttachDbFilename=E:\AppDb.mdf;Trusted_Connection=True;ConnectRetryCount=0
+         */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(connectionString);
+            
+            //.UseInMemoryDatabase("AppDb"); //Можно пользоваться бд в памяти, но для нашего примера не подойдёт
+        }
+
+        //Настраиваем модель данных
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Настраиваем маппинг пользователя в бд
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Name).IsRequired();
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            //Добавляем начальные данные (seed) в банку
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "John Snow"
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = "Sarah Connor"
+                    }
+                );
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Считываем строку подключения из аргументов командной строки
+             * По взрослому - из файла конфигурации считывать
+             */
+            var connectionString = args.FirstOrDefault();
+
+            //Все действия с бд выполняем в контексте
+            using (var db = new AppDb(connectionString))
+            {
+                //Пока просто выведем имена всех пользователей
+                var users = db.Users.Select(u => u.Name).ToList();
+                Console.WriteLine(string.Join('\n', users));
+            }
         }
     }
 }
